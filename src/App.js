9520189829a65f1c7cb36a32b185d99f364326b8
maddy <maddy.fries@protonmail.com>
2021-05-23 12:19:52 -0700

    1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+// imports
+import styled from "styled-components";
+var fs = require('fs');
+// logic
+const library = require("./plantNameLibrary");
+
+const File = styled.div`
+
+`;
+// not actually a grid
+const Grid = styled.div`
+  display: flex;
+`;
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  // justify-content: flex-start;
+  border: 2px solid;
+  margin: 5px;
+`;
+const BotanicalTitle = styled.h3`
+  margin: 2px;
+`;
+const Name = styled.p`
+  margin: 2px;
+`;
+
+function generateTable() {
+  console.log("starting plantTable.json generation");
+  let data = "{";
+  let link;
+  console.log(library.default);
+  library.default.forEach(plant => {
+    data += `"${plant.botanical}":"${plant.botanical}",`;
+    link = plant.botanical;
+    plant.linkedNames.forEach(name => {
+      data += `"${name}":"${link}",`
+    })
+  });
+  data += "}";
+  // regex
+  data = data.replace(/(?!")(?!:)(?!,)(?!{)(?!})\W+/gim, '-');
+  console.log(data);
+  // download
+  const a = document.createElement('a');
+  const file = new Blob([`${data}`], {type: 'text/plain'});
+  
+  a.href= URL.createObjectURL(file);
+  a.download = 'plantTable.json';
+  a.click();
+  
+  URL.revokeObjectURL(a.href);
+}
+
+function generateSuggestions() {
+  console.log("starting PlantSuggestion.js generation");
+  let data = "export default [";
+  console.log(library.default);
+  library.default.forEach(plant => {
+    data += `"${plant.botanical}", `;
+    plant.linkedNames.forEach(name => {
+      data += `"${name}", `;
+    })
+  });
+  data += "]";
+  console.log(data);
+  const a = document.createElement('a');
+  const file = new Blob([`${data}`], {type: 'text/plain'});
+  
+  a.href= URL.createObjectURL(file);
+  a.download = 'plantSuggestion.js';
+  a.click();
+  
+  URL.revokeObjectURL(a.href);
+}
 
 function App() {
+  console.log(library);
+  // Keep the .default it avoids a bug where i cant map because library is an object
+  const mapping = library.default.map((plant) => {
+    {
+      return (
+        <Card>
+          <BotanicalTitle>{plant.botanical}</BotanicalTitle>
+          <>
+            {plant.linkedNames.map((name) => {
+              {
+                return <Name>{name}</Name>;
+              }
+            })}
+          </>
+        </Card>
+      );
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <File>
+        <button onClick={generateTable}>
+          Generate plantTable.json
+        </button>
+        <button onClick={generateSuggestions}>
+          Generate plantSuggestion.js
+        </button>
+      </File>
+      <Grid>{mapping}</Grid>
     </div>
   );
 }
---
+++ src/plantNameLibrary.js
@@ -0,0 +1,23 @@
+// TEMPLATE array of objects
+/*
+    botanical:{variable}
+    {array of linked names}
+*/
+export default [
+    {
+        botanical:"dracaena trifasciata",
+        linkedNames:["sansevieria","sansevieria trifasciata","snake plant","mother in law's tongue","jinn's tongue","bow string hemp","snake tongue"]
+    },
+    {
+        botanical:"chamaedorea elegans",
+        linkedNames:["parlor palm","parlour palm","neanthe bella palm","tabletop palm","bella palm"]
+    },
+    {
+        botanical:"calathea lancifolia",
+        linkedNames:["rattlesnake plant","goeppertia insignis"]
+    },
+    {
+        botanical:"zamioculcas zamiifolia",
+        linkedNames:["zz plant","zanzibar gem","welcome plant","cardboard plant"]
+    }
+]
